ad2bb6fb633ba3c428a1fd4c801ef2136f4edc2c
Lisa Herzberg <lisamarieherzberg@gmail.com>
2018-04-18 10:48:53 -0700

    dice rolling

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-// const {Button, RefreshComposite, ImageView, ui, Composite, device, Picker, TextView, } = require('tabris');
+const {Button, ImageView, ui, Composite, device, Picker, TextView, } = require('tabris');
 
 const diceArray = [
   'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Dice-1-b.svg/2000px-Dice-1-b.svg.png',
@@ -9,170 +9,47 @@ const diceArray = [
 ]
 
 
-// // let rollDiceOne = new Button({
-// //   centerX: 0, centerY: -300,
-// //   text: 'Tap here'
-// // }).appendTo(ui.contentView);
-
-// // onRoll = false
-
-// // let setDice = () => {
-// //   onRoll = true
-// //   console.log(onRoll)
-// //   if (onRoll === true){
-// //     rollDice()
-// //     rollDiceTwo()
-// //   }else{
-// //     new ImageView({
-// //       left: 100, top: 'prev() 10', width: 200, height: 200,
-// //       background: 'white',
-// //       image: diceArray[Math.floor((Math.random() * Math.floor(4)))],
-// //       scaleMode: 'fill'
-// //     }).appendTo(ui.contentView)
-// //   }
-// // }
+let rollDice = new Button({
+  centerX: 0, centerY: 300,
+  text: 'Roll the Dice'
+}).appendTo(ui.contentView);
 
 
-// // let rollDice = () => {
-// //   new ImageView({
-// //     left: 100, top: 'prev() 10', width: 200, height: 200,
-// //     background: 'white',
-// //     image: diceArray[Math.floor((Math.random() * Math.floor(4)))],
-// //     scaleMode: 'fill'
-// //   }).appendTo(ui.contentView)
-// // }
-
-// // let rollDiceTwo = () => {
-// //   new ImageView({
-// //     left: 100, top: 'prev() 10', width: 200, height: 200,
-// //     background: 'white',
-// //     image: diceArray[Math.floor((Math.random() * Math.floor(4)))],
-// //     scaleMode: 'fill'
-// //   }).appendTo(ui.contentView)
-// // }
-
-// // rollDiceOne.on("select", setDice);
-
-
-// // Create composites and append children to them
-
-// let composite1 = new Composite({
-//   left: 0, top: '50%', bottom: 0, right: 0,
-//   background: '#f3f3f3'
-// }).appendTo(ui.contentView);
-
-// new ImageView({
-//     left: 100, top: 'prev() 10', width: 200, height: 200,
-//     background: 'white',
-//     image: diceArray[Math.floor((Math.random() * Math.floor(4)))],
-//     scaleMode: 'fill'
-//     }).appendTo(composite1)
-
-// let composite2 = new Composite({
-//   left: 0, top: 0, bottom: '50%', right: 0,
-//   background: '#f3f3f3'
-// }).appendTo(ui.contentView);
-
-// new ImageView({
-//   left: 100, top: 'prev() 10', width: 200, height: 200,
-//   background: 'white',
-//   image: diceArray[Math.floor((Math.random() * Math.floor(4)))],
-//   scaleMode: 'fill'
-//   }).appendTo(composite2)
-
-  const {RefreshComposite, ImageView, CheckBox, ScrollView, TextView, ui} = require('tabris');
-
-let refreshComposite = new RefreshComposite({
-  left: 0, right: 0, top: 0, bottom: 0,
-}).on('refresh', ({target}) => setTimeout(() => {
-  target.refreshIndicator = false;
-  imageView.image = diceArray[Math.floor((Math.random() * Math.floor(4)))];
-  imageViewTwo.image = diceArray[Math.floor((Math.random() * Math.floor(4)))];
-}, 1000)).appendTo(ui.contentView);
-
-let scrollView = new ScrollView({
-  left: 0, right: 0, top: 0, bottom: 0,
-}).appendTo(refreshComposite);
-
-// new ImageView({
-//   left: 100, top: 'prev() 10', width: 200, height: 200,
-//   background: 'white',
-//   image: diceArray[Math.floor((Math.random() * Math.floor(4)))],
-//   scaleMode: 'fill'
-//   }).appendTo(scrollView)
-
-  const imageView = 
-  new ImageView({
-    left: 100, top: 'prev() 10', width: 200, height: 200,
+let diceOne = new ImageView({
+    left: 100, top: 100, width: 200, height: 200,
     background: 'white',
     image: diceArray[Math.floor((Math.random() * Math.floor(4)))],
     scaleMode: 'fill'
-    }).appendTo(scrollView)
+  }).appendTo(ui.contentView)
 
-    const imageViewTwo = 
-    new ImageView({
-      left: 100, top: 'prev() 10', width: 200, height: 200,
-      background: 'white',
-      image: diceArray[Math.floor((Math.random() * Math.floor(4)))],
-      scaleMode: 'fill'
-      }).appendTo(scrollView)
-
-// new TextView({
-//   left: 0, right: 0, top: 32,
-//   alignment: 'center',
-//   font: 'black 24px',
-//   text: 'pull to refresh'
-// }).appendTo(scrollView);
-
-// const textView = new TextView({
-//   left: 0, right: 0, top: 'prev() 32',
-//   alignment: 'center',
-//   lineSpacing: 1.4,
-// }).appendTo(scrollView);
-
-new CheckBox({
-  left: 16, right: 16, bottom: 16,
-  text: 'Enable pull to refresh',
-  checked: true
-}).on('checkedChanged', ({value: checked}) => refreshComposite.refreshEnabled = checked)
-  .appendTo(ui.contentView);
+let diceTwo = new ImageView({
+  left: 100, top: 'prev() 10', width: 200, height: 200,
+  background: 'white',
+  image: diceArray[Math.floor((Math.random() * Math.floor(4)))],
+  scaleMode: 'fill'
+}).appendTo(ui.contentView)
 
 
-//   let rollDiceOne = new Button({
-//   centerX: 0, centerY: -300,
-//   text: 'Tap here'
+// rollDice.on('select', ({target}) =>  {
+//   diceOne.image = diceArray[Math.floor((Math.random() * Math.floor(4)))];
+//   diceTwo.image = diceArray[Math.floor((Math.random() * Math.floor(4)))];
 // }).appendTo(ui.contentView);
 
-// onRoll = false
 
-// let setDice = () => {
-//   onRoll = true
-//   console.log(onRoll)
-//   if (onRoll === true){
-//     rollDice()
-//     // rollDiceTwo()
-//   }else{
-//     new ImageView({
-//       left: 100, top: 'prev() 10', width: 200, height: 200,
-//       background: 'white',
-//       image: diceArray[Math.floor((Math.random() * Math.floor(4)))],
-//       scaleMode: 'fill'
-//     }).appendTo(ui.contentView)
-//   }
-// }
+rollDice.on('select', ({target}) =>  {
+  var arr = [500,700,900,1100,1300,1500,1700,1900,2100,2300]
+  for(let i=0; i< arr.length; i++){
+    console.log(arr[i])
+      setTimeout(() => {
+    rollTheDice(target)
+  }, arr[i])
+}}).appendTo(ui.contentView);
 
 
-// let rollDice = () => {
-//   dicer.image = diceArray[Math.floor((Math.random() * Math.floor(4)))]
-// }
+rollTheDice = (target) => {
+    console.log('fire?')
+    diceOne.image = diceArray[Math.floor((Math.random() * Math.floor(4)))];
+    diceTwo.image = diceArray[Math.floor((Math.random() * Math.floor(4)))];
+}
 
-// let rollDiceTwo = () => {
-//   new ImageView({
-//     left: 100, top: 'prev() 10', width: 200, height: 200,
-//     background: 'white',
-//     image: diceArray[Math.floor((Math.random() * Math.floor(4)))],
-//     scaleMode: 'fill'
-//   }).appendTo(ui.contentView)
-// }
-
-// rollDiceOne.on("select", setDice);
+// rollDice.on('select', ({target}) => rollTheDice(target)).appendTo(ui.contentView)
